d9dfedc923c02f63baea7827d36772c12b2fddc7
Istvan Nemeth <nemethistvanius@gmail.com>
2021-11-20 17:56:14 +0100

    refactor(client): add separate file for client functions

--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
-	"os"
-
-	"github.com/zmb3/spotify"
-	spotifyauth "github.com/zmb3/spotify/auth"
-	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
@@ -18,20 +12,4 @@ func main() {
 	}
 
 	fmt.Println(client)
-}
-
-func getSpotifyClient() (*spotify.Client, error) {
-	config := clientcredentials.Config{
-		ClientID:     os.Getenv("SPOTIFY_CLIENT_ID"),
-		ClientSecret: os.Getenv("SPOTIFY_CLIENT_SECRET"),
-		TokenURL:     spotifyauth.TokenURL,
-	}
-
-	token, err := config.Token(context.Background())
-	if err != nil {
-		return nil, fmt.Errorf("error retrieving token for spotify client: %v", err)
-	}
-
-	httpClient := spotifyauth.New().Client(context.Background(), token)
-	return spotify.New(httpClient), nil
 }
