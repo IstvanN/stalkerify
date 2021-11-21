aad1a92bea2ff7a7d6b7783baa131cb14d98579c
Istvan Nemeth <nemethistvanius@gmail.com>
2021-11-21 14:04:13 +0100

    feat(playlistinfo): add function to get playlistinfo by id

--- playlist_info.go
+++ playlist_info.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/globalsign/mgo/bson"
 	"github.com/zmb3/spotify"
 )
 
 var playlistID = os.Getenv("SPOTIFY_PLAYLIST_ID")
 
 type playListInfo struct {
-	ID             spotify.ID
-	Name           string `bson:"name"`
-	NumberOfTracks int    `bson:"numberOfTracks"`
-	Tracks         spotify.PlaylistTrackPage
+	ID             spotify.ID `bson:"id"`
+	Name           string     `bson:"name"`
+	NumberOfTracks int        `bson:"numberOfTracks"`
 }
 
 func createPlaylistInfoFromPlaylist(playlist *spotify.FullPlaylist) playListInfo {
@@ -20,6 +21,14 @@ func createPlaylistInfoFromPlaylist(playlist *spotify.FullPlaylist) playListInfo
 		ID:             playlist.ID,
 		Name:           playlist.Name,
 		NumberOfTracks: playlist.Tracks.Total,
-		Tracks:         playlist.Tracks,
 	}
+}
+
+func getPlaylistInfoByID(id spotify.ID) (playListInfo, error) {
+	var pi playListInfo
+	if err := getMongoCollection().Find(bson.M{"id": id}).One(&pi); err != nil {
+		return playListInfo{}, fmt.Errorf("error getting playlist: %v", err)
+	}
+
+	return pi, nil
 }
