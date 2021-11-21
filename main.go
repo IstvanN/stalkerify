faef34b4db9cab121ffd163c4639f2c008d45268
Istvan Nemeth <nemethistvanius@gmail.com>
2021-11-21 14:23:37 +0100

    feat(main): add base functionality to main

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+
+	"github.com/zmb3/spotify"
 )
 
 func main() {
+	db, err := startupMongo()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+	log.Println("successfully connected to MongoDB!")
+
 	client, err := getSpotifyClient()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	log.Println("successfully created Spotify client!")
 
-	fmt.Println(client)
+	playlist, err := client.GetPlaylist(context.Background(), spotify.ID(playlistID))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	currentPi, err := getPlaylistInfoByID(playlist.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if playlist.Tracks.Total > currentPi.NumberOfTracks {
+		log.Println("N E W S O N G F O U N D")
+		//TODO send mail functionality
+
+		if err := updatePlaylistInfo(currentPi, playlist); err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
